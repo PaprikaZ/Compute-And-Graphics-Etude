722a1fb2520847e89b851b2d86f8b682f60e87ab
PaprikaZ <without.plastic@outlook.com>
2022-08-29 09:41:08 +0800

    Add new struct initialization/logger console, with one time initialize guard

--- api-vulkan-tutorial-rust/entry-tutorial-main/src/initialization/mod.rs
+++ api-vulkan-tutorial-rust/entry-tutorial-main/src/initialization/mod.rs
@@ -1,1 +1,15 @@
-pub mod window;
+pub mod logger_console;
+pub mod window;
+
+use logger_console::InitializationLoggerConsole;
+
+
+pub struct Initialization {
+    pub logger_console: InitializationLoggerConsole,
+}
+
+impl Initialization {
+    pub fn create() -> Self {
+        Self { logger_console: InitializationLoggerConsole::new() }
+    }
+}
