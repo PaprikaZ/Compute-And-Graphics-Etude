dc92f7a98512a8bf652ca68452f92b13c9b04bce
PaprikaZ <without.plastic@outlook.com>
2022-09-06 14:24:01 +0800

    Update composition execution fn bind window event loop default, with window minimize/resize logic

--- api-vulkan-tutorial-rust/entry-tutorial-main/src/composition/execution.rs
+++ api-vulkan-tutorial-rust/entry-tutorial-main/src/composition/execution.rs
@@ -12,13 +12,22 @@ impl CompositionExecution {
      -> Result<(), TerminationProcessMain>
     {
         let mut be_destroying = false;
+        let mut be_minimized = false;
 
         window.event_loop.run(move |event, _, control_flow| {
             *control_flow = WindowUniformEventLoopControlFlow::Poll;
             match event {
-                WindowUniformEvent::MainEventsCleared if !be_destroying => {
+                WindowUniformEvent::MainEventsCleared if !be_destroying && !be_minimized => {
                     let _render_result = unsafe { application.render(&window.entity_main) };
                 },
+                WindowUniformEvent::WindowEvent { event: WindowUniformEventWindow::Resized(size), .. } =>
+                    match (size.width, size.height) {
+                        (0, 0) => { be_minimized = true; },
+                        _ => {
+                            be_minimized = false;
+                            application.signal_window_resized = true;
+                        }
+                    },
                 WindowUniformEvent::WindowEvent { event: WindowUniformEventWindow::CloseRequested, .. } => {
                     be_destroying = true;
                     *control_flow = WindowUniformEventLoopControlFlow::Exit;
