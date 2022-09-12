25302cfd48d62ed4cbd2deb74da4873fcfdabe02
PaprikaZ <without.plastic@outlook.com>
2022-09-12 16:27:28 +0800

    Add application update main 3d transform state action before rendering queue submit

--- api-vulkan-tutorial-rust/entry-tutorial-main/src/application/main.rs
+++ api-vulkan-tutorial-rust/entry-tutorial-main/src/application/main.rs
@@ -46,6 +46,7 @@ use crate::application::vulkan_frame_buffer::ApplicationVulkanFrameBuffer;
 use crate::application::vulkan_command_buffer::ApplicationVulkanCommandBuffer;
 use crate::application::vulkan_instance_validation_wi::ApplicationVulkanInstanceValidationWi;
 use crate::application::vulkan_instance_validation_wo::ApplicationVulkanInstanceValidationWo;
+use crate::application::evolution::ApplicationEvolution;
 use crate::application::vulkan_transform_d3_buffer::ApplicationVulkanTransformD3Buffer;
 
 
@@ -142,6 +143,8 @@ impl Application {
             }
         }
         self.vulkan_fence_s_in_flight_image[vulkan_next_image_index] = vulkan_slide_in_flight_fence;
+        //
+        ApplicationEvolution::update_state_transform_d3_main(self)?;
         let wait_vulkan_semaphore_s = &[self.vulkan_semaphore_s_image_available[self.vulkan_frame_index_current]];
         let wait_vulkan_pipeline_stage_flag_s = &[VulkanPipelineStageFlagS::COLOR_ATTACHMENT_OUTPUT];
         let vulkan_command_buffer_s = &[self.vulkan_command_buffer_s[vulkan_next_image_index]];
