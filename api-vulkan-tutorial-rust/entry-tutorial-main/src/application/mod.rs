1ab494de7776c2498836c8401a292eb3f4f45fe3
PaprikaZ <without.plastic@outlook.com>
2022-09-12 16:21:35 +0800

    Add new struct module application evolution with function update state transform 3d main

--- api-vulkan-tutorial-rust/entry-tutorial-main/src/application/mod.rs
+++ api-vulkan-tutorial-rust/entry-tutorial-main/src/application/mod.rs
@@ -17,4 +17,5 @@ pub mod vulkan_buffer;
 pub mod vulkan_memory;
 pub mod vulkan_vertex_index;
 pub mod vulkan_transform_d3_buffer;
-pub mod vulkan_transform_d3_descriptor;
+pub mod vulkan_transform_d3_descriptor;
+pub mod evolution;
