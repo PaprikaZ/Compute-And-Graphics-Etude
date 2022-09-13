f2429dc13265d8858130cc74c22da43f7e55a6cb
PaprikaZ <without.plastic@outlook.com>
2022-09-13 11:31:46 +0800

    Add new struct module config path, with new texture image path slice

--- api-vulkan-tutorial-rust/entry-tutorial-main/src/config/path.rs
+++ api-vulkan-tutorial-rust/entry-tutorial-main/src/config/path.rs
@@ -1,1 +1,18 @@
+use std::path::Path;
+use std::path::PathBuf;
+
+
 pub const DIRECTORY_ENTRY: &'static str = env!("CARGO_MANIFEST_DIR");
+pub const PATH_FILE_NAME_TEXTURE_MAIN: &'static str = "texture.png";
+pub const DIRECTORY_RELATIVE_ENTRY_RESOURCE: &'static str = "resource";
+
+
+pub struct ConfigPath {}
+
+impl ConfigPath {
+    pub unsafe fn get_file_texture_image_main() -> PathBuf {
+        Path::new(DIRECTORY_ENTRY)
+        .join(DIRECTORY_RELATIVE_ENTRY_RESOURCE)
+        .join(PATH_FILE_NAME_TEXTURE_MAIN)
+    }
+}
