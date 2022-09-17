384c1b19aba8a6f2800ee7b2522d0f50cb195c7d
PaprikaZ <without.plastic@outlook.com>
2022-09-17 14:42:07 +0800

    Add new application vulkan anti aliasing multisampling function get image memory view

--- api-vulkan-tutorial-rust/entry-tutorial-main/src/application/vulkan_anti_aliasing_multisampling.rs
+++ api-vulkan-tutorial-rust/entry-tutorial-main/src/application/vulkan_anti_aliasing_multisampling.rs
@@ -44,4 +44,36 @@ impl ApplicationVulkanAntiAliasingMultiSampling {
         .find(|c| frame_buffer_sample_count_flag_s.contains(*c))
         .unwrap_or(VulkanSampleCountFlagS::_1)
     }
+
+    pub unsafe fn get_image_memory_view(
+        vulkan_instance: &VulkanInstance,
+        vulkan_physical_device: VulkanDevicePhysical,
+        vulkan_logical_device: &VulkanDeviceLogical,
+        vulkan_swapchain_extent: VulkanExtentD2,
+        vulkan_swapchain_image_format: VulkanFormat,
+        vulkan_sample_count: VulkanSampleCountFlagS)
+     -> Result<(VulkanImage, VulkanDeviceMemory, VulkanImageView), TerminationProcessMain>
+    {
+        let (vulkan_multisampling_image, vulkan_multisampling_image_memory) =
+            ApplicationVulkanImage::create_with_memory(
+                vulkan_instance,
+                vulkan_physical_device,
+                vulkan_logical_device,
+                vulkan_swapchain_extent.width,
+                vulkan_swapchain_extent.height,
+                VulkanMipLevel::new(1),
+                vulkan_sample_count,
+                vulkan_swapchain_image_format,
+                VulkanImageTiling::OPTIMAL,
+                VulkanImageUsageFlagS::COLOR_ATTACHMENT | VulkanImageUsageFlagS::TRANSIENT_ATTACHMENT,
+                VulkanMemoryPropertyFlagS::DEVICE_LOCAL)?;
+        let vulkan_multisampling_image_view =
+            ApplicationVulkanImageView::create(
+                vulkan_logical_device,
+                vulkan_multisampling_image,
+                vulkan_swapchain_image_format,
+                VulkanImageAspectFlagS::COLOR,
+                VulkanMipLevel::new(1))?;
+        Ok((vulkan_multisampling_image, vulkan_multisampling_image_memory, vulkan_multisampling_image_view))
+    }
 }
