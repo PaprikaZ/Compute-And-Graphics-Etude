75c41ee3ec4cb7d9920cb5b16454094e496abf08
PaprikaZ <without.plastic@outlook.com>
2022-09-19 13:07:21 +0800

    Add new application evolution function update state transform 3d view projection

--- api-vulkan-tutorial-rust/entry-tutorial-main/src/application/evolution.rs
+++ api-vulkan-tutorial-rust/entry-tutorial-main/src/application/evolution.rs
@@ -8,13 +8,56 @@ use ::vulkan::VulkanMemoryMapFlagS;
 
 use crate::termination::TerminationProcessMain;
 use crate::application::main::Application;
+use crate::lib::transform_d3_view_projection::TransformD3ViewProjection;
 use crate::lib::transform_d3_model_view_projection::TransformD3ModelViewProjection;
 
 
 pub struct ApplicationEvolution {}
 
 impl ApplicationEvolution {
-    pub unsafe fn update_state_transform_d3_main(
+    pub unsafe fn update_state_transform_d3_view_projection(application: &Application)
+     -> Result<(), TerminationProcessMain>
+    {
+        let main_view_3d_transform =
+            glm::look_at(
+                &glm::vec3(2.0, 2.0, 2.0),
+                &glm::vec3(0.0, 0.0, 0.0),
+                &glm::vec3(0.0, 0.0, 1.0));
+        let mut main_projection_3d_transform =
+            glm::perspective_rh_zo(
+                application.vulkan_swapchain_extent.width as f32
+                / application.vulkan_swapchain_extent.height as f32,
+                glm::radians(&glm::vec1(45.0))[0],
+                0.1,
+                10.0);
+        main_projection_3d_transform[(1, 1)] *= -1.0;
+        let new_main_3d_transform =
+            TransformD3ViewProjection::new(
+                main_view_3d_transform,
+                main_projection_3d_transform);
+        //
+        let current_main_3d_transform_vulkan_buffer_memory =
+            application.vulkan_transform_d3_main_buffer_memory_s[application.vulkan_frame_index_current];
+        let map_current_frame_main_3d_transform_vulkan_buffer_memory_address_result =
+            application.vulkan_device_logical.map_memory(
+                current_main_3d_transform_vulkan_buffer_memory,
+                0,
+                size_of::<TransformD3ModelViewProjection>() as u64,
+                VulkanMemoryMapFlagS::empty());
+        let current_frame_main_3d_transform_vulkan_buffer_memory_address =
+            match map_current_frame_main_3d_transform_vulkan_buffer_memory_address_result {
+                Err(error) => {
+                    let vulkan_error_code = VulkanErrorCode::new(error.as_raw());
+                    return Err(TerminationProcessMain::InitializationVulkanMemoryMapFail(vulkan_error_code));
+                },
+                Ok(address) => address,
+            };
+        copy_nonoverlapping(&new_main_3d_transform, current_frame_main_3d_transform_vulkan_buffer_memory_address.cast(), 1);
+        application.vulkan_device_logical.unmap_memory(current_main_3d_transform_vulkan_buffer_memory);
+        Ok(())
+    }
+
+    pub unsafe fn update_state_transform_d3_model_view_projection(
         application: &Application
     ) -> Result<(), TerminationProcessMain>
     {
@@ -63,7 +106,5 @@ impl ApplicationEvolution {
         copy_nonoverlapping(&new_main_3d_transform, current_frame_main_3d_transform_vulkan_buffer_memory_address.cast(), 1);
         application.vulkan_device_logical.unmap_memory(current_main_3d_transform_vulkan_buffer_memory);
         Ok(())
-
-
     }
 }
