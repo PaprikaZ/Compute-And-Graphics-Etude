8eec6d884b4a9db7d4aadc9bf1c0cf9268bbe44a
WithoutPlastic <without.plastic@outlook.com>
2024-08-29 06:49:01 +0000

    Add new struct vulkan swapchain image index

--- api-vulkan-guide-rust/source-main/library_foundation_reintroduction/src/vulkan/swapchain.rs
+++ api-vulkan-guide-rust/source-main/library_foundation_reintroduction/src/vulkan/swapchain.rs
@@ -10,3 +10,17 @@ impl VulkanSwapchainImageNumber {
         self.0
     }
 }
+
+
+#[derive(Debug, Clone, Copy)]
+pub struct VulkanSwapchainImageIndex(u32);
+
+impl VulkanSwapchainImageIndex {
+    pub fn new(image_index: u32) -> Self {
+        Self(image_index)
+    }
+
+    pub fn as_raw(self) -> u32 {
+        self.0
+    }
+}
