a2746a4d24be1592c4decd462595513a15a9ce70
WithoutPlastic <without.plastic@outlook.com>
2024-08-30 14:20:38 +0000

    Add new struct vulkan requirement version api least

--- api-vulkan-guide-rust/source-main/library_foundation_vulkan_cooked/src/error/foundation_vulkan_cooked.rs
+++ api-vulkan-guide-rust/source-main/library_foundation_vulkan_cooked/src/error/foundation_vulkan_cooked.rs
@@ -1,5 +1,7 @@
 #[derive(Debug)]
 pub enum ErrorFoundationVulkanCookedOwn {
+    VulkanRequirementVersionApiLeastInstanceNotFulfilled,
+    VulkanRequirementVersionApiLeastDevicePhysicalNotFulfilled,
 }
 
 
