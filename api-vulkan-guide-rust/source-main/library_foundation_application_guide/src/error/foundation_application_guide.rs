755d124cb202d2cf5834e1ad80a6df1ee97a7790
WithoutPlastic <without.plastic@outlook.com>
2024-09-01 03:58:25 +0000

    Add new module error foundation application guide, empty

---
+++ api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/error/foundation_application_guide.rs
@@ -0,0 +1,53 @@
+use ::library_foundation_reintroduction::error::foundation_reintroduction::ErrorFoundationReintroductionOwn;
+use ::library_foundation_reintroduction::error::foundation_reintroduction::ErrorFoundationReintroduction;
+use ::library_foundation_vulkan_cooked::error::foundation_vulkan_cooked::ErrorFoundationVulkanCookedOwn;
+use ::library_foundation_vulkan_cooked::error::foundation_vulkan_cooked::ErrorFoundationVulkanCooked;
+
+
+#[derive(Debug)]
+pub enum ErrorFoundationApplicationGuideOwn {
+}
+
+
+#[derive(Debug)]
+pub enum ErrorFoundationApplicationGuide {
+    Own(ErrorFoundationApplicationGuideOwn),
+    ScopeReintroduction(ErrorFoundationReintroductionOwn),
+    ScopeVulkanCooked(ErrorFoundationVulkanCookedOwn),
+}
+
+impl From<ErrorFoundationApplicationGuideOwn> for ErrorFoundationApplicationGuide {
+    fn from(error: ErrorFoundationApplicationGuideOwn) -> Self {
+        Self::Own(error)
+    }
+}
+
+impl From<ErrorFoundationReintroduction> for ErrorFoundationApplicationGuide {
+    fn from(error: ErrorFoundationReintroduction) -> Self {
+        match error {
+            ErrorFoundationReintroduction::Own(e) =>
+                Self::ScopeReintroduction(e),
+        }
+    }
+}
+
+impl From<ErrorFoundationReintroductionOwn> for ErrorFoundationApplicationGuide {
+    fn from(error: ErrorFoundationReintroductionOwn) -> Self {
+        Self::ScopeReintroduction(error)
+    }
+}
+
+impl From<ErrorFoundationVulkanCooked> for ErrorFoundationApplicationGuide {
+    fn from(error: ErrorFoundationVulkanCooked) -> Self {
+        match error {
+            ErrorFoundationVulkanCooked::Own(e) =>
+                Self::ScopeVulkanCooked(e),
+        }
+    }
+}
+
+impl From<ErrorFoundationVulkanCookedOwn> for ErrorFoundationApplicationGuide {
+    fn from(error: ErrorFoundationVulkanCookedOwn) -> Self {
+        Self::ScopeVulkanCooked(error)
+    }
+}
---
+++ api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/error/mod.rs
@@ -0,0 +1,1 @@
+pub mod foundation_application_guide;
