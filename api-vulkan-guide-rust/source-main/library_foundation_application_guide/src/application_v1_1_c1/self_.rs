b112e826ba1659d68398f5ec94a6afe8f6c4890b
WithoutPlastic <without.plastic@outlook.com>
2024-09-04 03:34:53 +0000

    Implement application part window, private field with public get fn

--- api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c1/self_.rs
+++ api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c1/self_.rs
@@ -6,14 +6,30 @@ use ::library_foundation_reintroduction::vulkan::VulkanEntry;
 
 #[derive(Debug)]
 pub struct ApplicationPartWindow {
-    pub window_event_loop: WindowUniformEventLoop<()>,
-    pub window: WindowUniformWindow,
+    window_event_loop: WindowUniformEventLoop<()>,
+    window: WindowUniformWindow,
 }
 
 impl ApplicationPartWindow {
-    pub fn terminate(&self)
+    pub fn new(window: WindowUniformWindow, window_event_loop: WindowUniformEventLoop<()>)
+    -> Self
     {
-        todo!()
+        Self {
+            window: window,
+            window_event_loop: window_event_loop,
+        }
+    }
+
+    pub fn as_raw(self) -> (WindowUniformWindow, WindowUniformEventLoop<()>) {
+        (self.window, self.window_event_loop)
+    }
+
+    pub fn get_window(&self) -> &WindowUniformWindow {
+        &self.window
+    }
+
+    pub fn get_window_event_loop(&self) -> &WindowUniformEventLoop<()> {
+        &self.window_event_loop
     }
 }
 
