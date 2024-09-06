5aeec6bf1d15d7bb097fa86797b21d2b107e69f0
WithoutPlastic <without.plastic@outlook.com>
2024-09-06 08:30:41 +0000

    Add new config vulkan base, fn create, with extra merge mixin step

--- api-vulkan-guide-rust/source-main/library_foundation_vulkan_cooked/src/config_vulkan/base.rs
+++ api-vulkan-guide-rust/source-main/library_foundation_vulkan_cooked/src/config_vulkan/base.rs
@@ -68,6 +68,66 @@ pub struct ConfigVulkanBase<'t> {
 }
 
 impl<'t> ConfigVulkanBase<'t> {
+    fn merge_mixin_instance_layer_extension(
+        instance_layer_extension_mixin: ConfigVulkanBaseMixinInstanceLayerExtension,
+        required_instance_layer_name_s: &mut HashSet<VulkanLayerName>,
+        required_instance_extension_name_s: &mut HashSet<VulkanExtensionName>)
+    {
+        instance_layer_extension_mixin.instance_layer_name_s
+        .iter()
+        .for_each(|n| { let _ = required_instance_layer_name_s.insert(*n); });
+        instance_layer_extension_mixin.instance_extension_name_s
+        .iter()
+        .for_each(|n| { let _ = required_instance_extension_name_s.insert(*n); });
+    }
+
+    pub fn create(
+        least_api_version_requirement: VulkanRequirementVersionApiLeast,
+        engine_name: VulkanEngineName<'t>,
+        engine_version: VulkanVersionEngine,
+        application_name: VulkanApplicationName<'t>,
+        application_version: VulkanVersionApplication,
+        mut required_instance_layer_name_s: HashSet<VulkanLayerName>,
+        optional_instance_layer_name_s: HashSet<VulkanLayerName>,
+        instance_window_extension_name_s: HashSet<VulkanLayerName>,
+        mut required_instance_extension_name_s: HashSet<VulkanExtensionName>,
+        optional_instance_extension_name_s: HashSet<VulkanExtensionName>,
+        required_physical_device_extension_name_s: HashSet<VulkanExtensionName>,
+        optional_physical_device_extension_name_s: HashSet<VulkanExtensionName>,
+        required_physical_device_feature_name_s: HashSet<VulkanDevicePhysicalFeatureStandardName>,
+        optional_physical_device_feature_name_s: HashSet<VulkanDevicePhysicalFeatureStandardName>,
+        debug_utility_extension_message_type_flag_s: VulkanExtensionDebugUtilityMessageTypeFlagS,
+        debug_utility_extension_message_severity_flag_s: VulkanExtensionDebugUtilityMessageSeverityFlagS,
+        be_to_enable_validation: VulkanValidationBeToEnable)
+    -> Self
+    {
+        let instance_layer_extension_mixin =
+            ConfigVulkanBaseMixinInstanceLayerExtension::create(be_to_enable_validation);
+        Self::merge_mixin_instance_layer_extension(
+            instance_layer_extension_mixin,
+            &mut required_instance_layer_name_s, &mut required_instance_extension_name_s);
+        Self {
+            version_api_least_requirement: least_api_version_requirement,
+            engine_name: engine_name,
+            engine_version: engine_version,
+            application_name: application_name,
+            application_version: application_version,
+            instance_create_flag_s: VulkanInstanceCreateFlagS::empty(),
+            instance_layer_name_s_required: required_instance_layer_name_s,
+            instance_layer_name_s_optional: optional_instance_layer_name_s,
+            instance_extension_window_name_s: instance_window_extension_name_s,
+            instance_extension_name_s_required: required_instance_extension_name_s,
+            instance_extension_name_s_optional: optional_instance_extension_name_s,
+            device_physical_extension_name_s_required: required_physical_device_extension_name_s,
+            device_physical_extension_name_s_optional: optional_physical_device_extension_name_s,
+            device_physical_feature_name_s_required: required_physical_device_feature_name_s,
+            device_physical_feature_name_s_optional: optional_physical_device_feature_name_s,
+            extension_debug_utility_message_type_flag_s: debug_utility_extension_message_type_flag_s,
+            extension_debug_utility_message_severity_flag_s: debug_utility_extension_message_severity_flag_s,
+            be_to_enable_validation: be_to_enable_validation,
+        }
+    }
+
     pub fn create_vulkan_application_information(
         &self, negotiated_api_version: &VulkanVersionApi)
     -> VulkanApplicationInformation
