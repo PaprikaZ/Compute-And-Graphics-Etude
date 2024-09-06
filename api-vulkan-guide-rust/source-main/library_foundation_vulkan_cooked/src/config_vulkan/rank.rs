312593bf64d98aed4e8bc29b419525ccd049604f
WithoutPlastic <without.plastic@outlook.com>
2024-09-06 08:03:35 +0000

    Rename config vulkan rank, field weight factor device physical version api least minor exceeding point

--- api-vulkan-guide-rust/source-main/library_foundation_vulkan_cooked/src/config_vulkan/rank.rs
+++ api-vulkan-guide-rust/source-main/library_foundation_vulkan_cooked/src/config_vulkan/rank.rs
@@ -6,7 +6,7 @@ use crate::vulkan_rank::VulkanRank;
 pub struct ConfigVulkanRank {
     pub weight_factor_device_physical_type_gpu_discrete: VulkanRankWeightFactorExponential,
     pub weight_factor_device_physical_type_gpu_integrated: VulkanRankWeightFactorExponential,
-    pub weight_factor_version_api_least_minor_exceeding_point: VulkanRankWeightFactorExponential,
+    pub weight_factor_device_physical_version_api_least_minor_exceeding_point: VulkanRankWeightFactorExponential,
     pub weight_factor_device_physical_extension_one: VulkanRankWeightFactorExponential,
     pub weight_factor_device_physical_feature_one: VulkanRankWeightFactorExponential,
 }
@@ -16,7 +16,7 @@ impl ConfigVulkanRank {
         VulkanRank::new(
             self.weight_factor_device_physical_type_gpu_discrete,
             self.weight_factor_device_physical_type_gpu_integrated,
-            self.weight_factor_version_api_least_minor_exceeding_point,
+            self.weight_factor_device_physical_version_api_least_minor_exceeding_point,
             self.weight_factor_device_physical_extension_one,
             self.weight_factor_device_physical_feature_one)
     }
