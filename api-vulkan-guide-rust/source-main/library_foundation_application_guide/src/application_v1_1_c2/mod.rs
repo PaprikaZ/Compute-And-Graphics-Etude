802301addddb829b85bdea8f9c286632ee4ed528
WithoutPlastic <without.plastic@outlook.com>
2024-09-08 08:16:13 +0000

    Add new enum application scene name

--- api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c2/mod.rs
+++ api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c2/mod.rs
@@ -3,6 +3,7 @@ pub mod vulkan_debug;
 pub mod negotiation_vulkan_device_physical;
 pub mod negotiation_vulkan_swapchain;
 pub mod graphic_resource;
+pub mod scene;
 pub mod self_;
 pub mod initialization;
 pub mod continuation;
---
+++ api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c2/scene.rs
@@ -0,0 +1,8 @@
+#[derive(Debug, Clone, Copy)]
+pub enum ApplicationSceneName {
+    TriangleRed,
+    TriangleColored,
+}
+
+impl ApplicationSceneName {
+}
