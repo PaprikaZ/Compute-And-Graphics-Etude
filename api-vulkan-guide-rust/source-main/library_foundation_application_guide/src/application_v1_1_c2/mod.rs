81a267d9097bbf7029419dd0c85eeb2e3357ec0d
WithoutPlastic <without.plastic@outlook.com>
2024-09-08 07:54:57 +0000

    Add new enum/struct application graphic resource destroy directive/stack

--- api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c2/mod.rs
+++ api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c2/mod.rs
@@ -2,6 +2,7 @@ pub mod config;
 pub mod vulkan_debug;
 pub mod negotiation_vulkan_device_physical;
 pub mod negotiation_vulkan_swapchain;
+pub mod graphic_resource;
 pub mod self_;
 pub mod initialization;
 pub mod continuation;
