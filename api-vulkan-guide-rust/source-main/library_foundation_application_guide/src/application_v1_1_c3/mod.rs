e31dd41aea60ec6d15743ed17b054fb78c7e2505
WithoutPlastic <without.plastic@outlook.com>
2024-09-11 10:50:57 +0000

    Add new struct application vulkan push constant data, with fn create

--- api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c3/mod.rs
+++ api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c3/mod.rs
@@ -5,6 +5,7 @@ pub mod negotiation_vulkan_swapchain;
 pub mod graphic_resource;
 pub mod graphic_mesh;
 pub mod scene;
+pub mod vulkan_push_constant;
 pub mod self_;
 pub mod initialization;
 pub mod continuation;
