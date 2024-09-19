6559d19e5f84dab691f5eff15a374e7d5c2a43c8
WithoutPlastic <without.plastic@outlook.com>
2024-09-19 13:52:10 +0000

    Add new struct application scene entity renderable, enum application scene entity renderable name

--- api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c3_scene/scene.rs
+++ api-vulkan-guide-rust/source-main/library_foundation_application_guide/src/application_v1_1_c3_scene/scene.rs
@@ -1,5 +1,8 @@
+use ::library_foundation_reintroduction::nalgebra_glm as glm;
 use ::library_foundation_reintroduction::vulkan::VulkanPipeline;
 use ::library_foundation_reintroduction::vulkan::VulkanPipelineLayout;
+
+use crate::application_v1_1_c3_scene::graphic_mesh::ApplicationGraphicMeshName;
 
 
 #[derive(Debug)]
@@ -25,4 +28,40 @@ pub enum ApplicationScenePipelineName {
 }
 
 impl ApplicationScenePipelineName {
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum ApplicationSceneEntityRenderableName {
+    Triangle(u32),
+    Monkey,
+}
+
+impl ApplicationSceneEntityRenderableName {
+}
+
+
+#[derive(Debug, Clone)]
+pub struct ApplicationSceneEntityRenderable {
+    pub name: ApplicationSceneEntityRenderableName,
+    pub pipeline_name: ApplicationScenePipelineName,
+    pub graphic_mesh_name: ApplicationGraphicMeshName,
+    pub graphic_transform: glm::Mat4,
+}
+
+impl ApplicationSceneEntityRenderable {
+    pub fn new(
+        name: ApplicationSceneEntityRenderableName,
+        pipeline_name: ApplicationScenePipelineName,
+        graphic_mesh_name: ApplicationGraphicMeshName,
+        graphic_transform: glm::Mat4)
+    -> Self
+    {
+        Self {
+            name: name,
+            pipeline_name: pipeline_name,
+            graphic_mesh_name,
+            graphic_transform: graphic_transform,
+        }
+    }
 }
